463a95f25afb7b6ebedbe27ff158c388f8984691
NB106 <NB106@users.noreply.github.com>
2018-10-10 19:53:51 +0930

    new lyaout with add create list
    as my previous layout was not working for sub task adding, i created new layout

--- js/main.js
+++ js/main.js
@@ -1,178 +1,80 @@
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')) : {
-  todo: [],
-  completed: [],
-  ongoing: []
-};
+(function() {
+  'use strict'
 
-// Remove and complete icons in SVG format
-var addsubSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve"><g><path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/></g></svg>'
-var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6V18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>';
-var ongoingSVG = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>';
-var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
+  //Board constructor object and assign some properties to its prototype
+  function TaskBoard(title) {
+    var nextId = 0
 
-renderTodoList();
+    this.title = title
+    this.lists = []
+    this.cards = {}
 
-// User clicked on the add button
-// If there is any text inside the item field, add that text to the todo list
-document.getElementById('add').addEventListener('click', function() {
-  var value = document.getElementById('item').value;
-  if (value) {
-    addItem(value);
-  }
-});
+    this.node = document.createElement('div')
+    this.titleNode = document.createElement('div')
+    this.listsNode = document.createElement('div')
 
-document.getElementById('item').addEventListener('keydown', function(e) {
-  var value = this.value;
-  if ((e.code === 'Enter' || e.code === 'NumpadEnter') && value) {
-    addItem(value);
-  }
-});
+    this.node.id = 'board'
+    this.titleNode.id = 'todo-title-board'
+    this.listsNode.id = 'todo-canvas-board'
 
-function addItem(value) {
-  addItemToDOM(value);
-  document.getElementById('item').value = '';
+    // new list title form
+    this.titleFormNode = buildListTitleForm()
+    this.titleNode.appendChild(document.createTextNode(this.title))
 
-  data.todo.push(value);
-  dataObjectUpdated();
-}
-
-function renderTodoList() {
-  if (!data.todo.length && !data.completed.length && !data.ongoing.length) return;
-
-  for (var i = 0; i < data.todo.length; i++) {
-    var value = data.todo[i];
-    addItemToDOM(value);
+    this.getNextId = function() {
+      return '_' + (nextId++).toString()
+    }
   }
 
-  for (var j = 0; j < data.completed.length; j++) {
-    var value = data.completed[j];
-    addItemToDOM(value, true);
+  TaskBoard.prototype.render = function() {
+    this.lists.push(new List(this, 'Add a new task', 0, true))
+    for (var i = 0; i < this.lists.length; ++i) {
+      this.listsNode.appendChild(this.lists[i].node)
+    }
+    this.lists[this.lists.length - 1].node.appendChild(this.titleFormNode)
+    this.lists[this.lists.length - 1].titleNode.onclick = addListTrello(this)
+    this.node.appendChild(this.titleNode)
+    this.node.appendChild(this.listsNode)
   }
 
-  for (var k = 0; k < data.ongoing.length; k++) {
-    var value = data.ongoing[k];
-    addItemToDOM(value, true);
+
+  //Onloading the document render the board.The code starts from here
+  document.body.onload = function() {
+    var title = 'My things ToDo',
+      board = new TaskBoard(title)
+
+    board.render()
+    document.getElementById('container').appendChild(board.node)
+    currentBoard = board
   }
-}
+}())
 
-function dataObjectUpdated() {
-  localStorage.setItem('todoList', JSON.stringify(data));
-}
+//This function will called on adding the list on the board
+function addListTrello(board) {
+  return function() {
+    var titleInput = document.getElementById('trello-list-title-input')
 
-function removeItem() {
-  var item = this.parentNode.parentNode;
-  var parent = item.parentNode;
-  var id = parent.id;
-  var value = item.innerText;
+    document.getElementById('trello-list-title-submit').onclick = titleButtonClick
+    board.titleFormNode.style.display = 'block'
+    titleInput.focus()
 
-  if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-  } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
+    function titleButtonClick(evt) {
+      evt.preventDefault()
+      var title = titleInput.value.trim(),
+        index = board.lists.length - 1,
+        list
+
+      board.titleFormNode.style.display = 'none'
+      titleInput.value = ''
+      if (!title) {
+        return
+      }
+
+      list = new List(board, title, index)
+      board.lists.splice(index, 0, list)
+      board.listsNode.insertBefore(list.node,
+        board.lists[index + 1].node)
+      board.lists[index + 1].titleNode.setAttribute('list-index', index + 1)
+    }
   }
-  dataObjectUpdated();
-
-  parent.removeChild(item);
-}
-
-function completeItem() {
-  var item = this.parentNode.parentNode;
-  var parent = item.parentNode;
-  var id = parent.id;
-  var value = item.innerText;
-
-  if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-    data.completed.push(value);
-  } else {
-    data.completed.splice(data.completed.indexOf(value), 1);
-    data.todo.push(value);
-  }
-  dataObjectUpdated();
-
-  // Check if the item should be added to the completed list or to re-added to the todo list
-  var target = (id === 'todo') ? document.getElementById('completed') : document.getElementById('todo');
-
-  parent.removeChild(item);
-  target.insertBefore(item, target.childNodes[0]);
-}
-
-function ongoingItem() {
-
-  var item = this.parentNode.parentNode;
-  var parent = item.parentNode;
-  var id = parent.id;
-  var value = item.innerText;
-
-  if (id === 'todo') {
-    data.todo.splice(data.todo.indexOf(value), 1);
-    data.ongoing.push(value);
-  } else {
-    data.ongoing.splice(data.ongoing.indexOf(value), 1);
-    data.todo.push(value);
-  }
-  dataObjectUpdated();
-
-  var target = (id === 'todo') ? document.getElementById('ongoing') : document.getElementById('todo');
-
-  parent.removeChild(item);
-  target.insertBefore(item, target.childNodes[0]);
-
-}
-
-function subItem() {
-  var item = this.parentNode.parentNode;
-  var mi = document.createElement("input");
-  mi.setAttribute('type', 'text');
-  mi.setAttribute('value', 'default');
-}
-
-// Adds a new item to the todo list
-function addItemToDOM(text, completed) {
-  var list = (completed) ? document.getElementById('completed') : document.getElementById('todo');
-
-  var item = document.createElement('li');
-  item.innerText = text;
-
-  var buttons = document.createElement('div');
-  buttons.classList.add('buttons');
-
-  var remove = document.createElement('button');
-  remove.classList.add('remove');
-  remove.innerHTML = removeSVG;
-
-  // Add click event for removing the item
-  remove.addEventListener('click', removeItem);
-
-  var complete = document.createElement('button');
-  complete.classList.add('complete');
-  complete.innerHTML = completeSVG;
-
-  // Add click event for completing the item
-  complete.addEventListener('click', completeItem);
-
-
-
-  var addsub = document.createElement('button');
-  addsub.classList.add('addsub');
-  addsub.innerHTML = addsubSVG;
-
-  // Add click event for adding the sub item.
-  addsub.addEventListener('click', subItem);
-
-  var ongoing = document.createElement('button');
-  ongoing.classList.add('ongoing');
-  ongoing.innerHTML = ongoingSVG;
-
-  ongoing.addEventListener('click', ongoingItem);
-
-
-
-  buttons.appendChild(remove);
-  buttons.appendChild(complete);
-  buttons.appendChild(addsub);
-  buttons.appendChild(ongoing);
-  item.appendChild(buttons);
-
-  list.insertBefore(item, list.childNodes[0]);
 }
