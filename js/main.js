75630b75632e1110e85b42d696e10a3d2bc2bbce
NB106 <NB106@users.noreply.github.com>
2018-10-11 20:30:10 +0930

    sub task added

--- js/main.js
+++ js/main.js
@@ -38,6 +38,33 @@
   }
 
 
+  TaskBoard.prototype.registerCard = function(card, index) {
+    this.cards[card.id] = {
+      card: card,
+      list: card.list,
+      index: index
+    }
+  }
+
+  TaskBoard.prototype.reregisterSubsequent = function(list, index, shift) {
+    for (var i = index; i < list.cards.length; ++i) {
+      this.registerCard(list.cards[i], i + shift)
+    }
+  }
+
+  TaskBoard.prototype.unregisterCard = function(card) {
+    delete this.cards[card.id]
+  }
+
+
+  //if you click on escape then also the edit window will get closed
+  window.onkeydown = function(evt) {
+    if (evt.keyCode === 27) {
+      cardEdit.close()
+    }
+  }
+
+
   //Onloading the document render the board.The code starts from here
   document.body.onload = function() {
     var title = 'My things ToDo',
@@ -48,6 +75,8 @@
     currentBoard = board
   }
 }())
+
+
 
 //This function will called on adding the list on the board
 function addListTodo(board) {
@@ -75,6 +104,133 @@ function addListTodo(board) {
       board.listsNode.insertBefore(list.node,
         board.lists[index + 1].node)
       board.lists[index + 1].titleNode.setAttribute('list-index', index + 1)
+    }
+  }
+}
+
+//add list
+function List(board, title, index, dummyList) {
+  this.board = board
+  this.dummyList = dummyList
+  this.title = title
+  this.index = index
+  this.node = document.createElement('div')
+  this.titleNode = document.createElement('div')
+  this.cardsNode = document.createElement('div')
+  this.node.classList.add('list')
+  this.titleNode.classList.add('list-title')
+  this.cardsNode.classList.add('list-cards')
+  this.titleNode.setAttribute('list-index', index)
+  this.titleNode.appendChild(document.createTextNode(this.title))
+  this.node.appendChild(this.titleNode)
+
+  if (!dummyList) {
+    var dummyCard = new Card(this, 'Add a subtask...', 0)
+
+    this.cards = [dummyCard]
+    board.registerCard(this.cards[0], 0)
+
+    // new card title form
+    this.titleFormNode = buildCardTitleForm()
+
+    for (var i = 0; i < this.cards.length; ++i) {
+      this.cardsNode.appendChild(this.cards[i].node)
+    }
+    dummyCard.titleNode.onclick = addCardTodo(this)
+    this.node.appendChild(this.cardsNode)
+    dummyCard.node.appendChild(this.titleFormNode)
+    dummyCard.node.draggable = false
+    dummyCard.node.onclick = undefined
+  }
+}
+
+/*
+This function will build the form for list,It is called by addList
+ */
+function buildListTitleForm() {
+  var node = document.createElement('form')
+  node.innerHTML =
+    '<div class="newitem-title-wrapper">' +
+    '<input id="todo-list-title-input" type="text">' +
+    '<input id="todo-list-title-submit" type="submit" value="Add">' +
+    '</div>'
+  node.style.display = 'none'
+  return node
+}
+
+//this function will build the card node
+function buildCardNode() {
+  var node = document.createElement('div')
+  node.draggable = true
+  node.innerHTML =
+    '<div class="card-title"></div>';
+  return node
+}
+
+/*
+ This function is constructor function for card
+ */
+function Card(list, title) {
+  this.id = list.board.getNextId()
+  this.list = list
+  this.title = title
+  this.node = buildCardNode()
+  this.titleNode = this.node.getElementsByClassName('card-title')[0]
+
+  this.node.classList.add('card')
+  this.node.setAttribute('card-id', this.id)
+  this.titleNode.appendChild(document.createTextNode(this.title))
+
+
+  // this function will be called once you click on the text to edit
+  this.node.onclick = (function(card) {
+    return function() {
+      cardEdit.card = card
+      cardEdit.titleNode.value = card.title;
+      cardEdit.show()
+    }
+  }(this))
+}
+
+function buildCardTitleForm() {
+  var node = document.createElement('form')
+  node.innerHTML =
+    '<div class="newitem-title-wrapper">' +
+    '<textarea class="todo-new-card-title-input" type="text"></textarea>' +
+    '<input class="todo-new-card-title-submit" type="submit" value="Add">' +
+    '</div>'
+  node.style.display = 'none'
+  return node
+}
+
+/*
+ This function will add the Card in the list
+ */
+
+function addCardTodo(list) {
+  return function() {
+    var titleTextarea = list.titleFormNode
+      .getElementsByClassName('todo-new-card-title-input')[0]
+    list.titleFormNode.getElementsByClassName('todo-new-card-title-submit')[0]
+      .onclick = titleSubmit
+    list.titleFormNode.style.display = 'block';
+    titleTextarea.focus();
+
+    function titleSubmit(evt) {
+      evt.preventDefault()
+      var title = titleTextarea.value.trim(),
+        card;
+
+      list.titleFormNode.style.display = 'none';
+      titleTextarea.value = '';
+      if (!title) {
+        return
+      }
+
+      card = new Card(list, title);
+      list.board.registerCard(card, list.cards.length);
+      list.cardsNode.insertBefore(card.node, list.cards[list.cards.length - 1].node);
+      list.cards.push(card);
     }
   }
 }
